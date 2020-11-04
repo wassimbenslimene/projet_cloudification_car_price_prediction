30fb11686221dc716659658c2d611031de9646eb
wassim ben slimene <wassim.benslimene@supcom.tn>
2020-11-04 23:00:36 +0100

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "Standard_DS1_v2"
+    vm_size         = "Standard_A2_v2"
   }
 
   service_principal {
